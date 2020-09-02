3045f12ef33cd75456b8fd320883d68ef06624f5
Joel Fernandes (Google) <joel@joelfernandes.org>
2020-09-02 13:37:26 -0400

    Fix include errors due to unneeded rcupdate.h
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>

--- check.sh
+++ check.sh
@@ -5,7 +5,6 @@ trap 'rm -f $F' 0
 
 mkdir include/linux -p
 cp /usr/local/google/home/joelaf/repo/linux-master/include/linux/rcu_segcblist.h include/linux
-cp /usr/local/google/home/joelaf/repo/linux-master/include/linux/rcupdate.h include/linux
 cp /usr/local/google/home/joelaf/repo/linux-master/include/linux/atomic.h include/linux
 cp /usr/local/google/home/joelaf/repo/linux-master/kernel/rcu/rcu_segcblist.h rcu_segcblist.h
 cp /usr/local/google/home/joelaf/repo/linux-master/kernel/rcu/rcu_segcblist.c rcu_segcblist.c
